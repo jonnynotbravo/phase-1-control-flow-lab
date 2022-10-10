1ec250009d0a6aa1e7e5b7dd884ec6136e21af31
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-10 19:58:20 -0400

    commit tree 8d6bd886d7c89732307f7eaeb91c263ff7948e91

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+
   // Write your code here!
+  if(ride < 401){
+
+    return 'This one is on me!';
+  }
+
+  if(ride < 2001 && ride > 399){
+
+    return 'That will be twenty bucks.'
+  }
+
+  if(ride >= 2000 && ride <= 2500){
+
+    return 'I will gladly take your thirty bucks.';
+  }
+
+
+  return 'No can do.';
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    
+    default: 
+      return 'Bye.';
+      break;
+  }
+
 }
